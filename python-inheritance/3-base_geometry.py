e0eec7bddd13acc5c7c648f77b664982316c6abd
blen <blenhenok93@gmail.com>
2023-08-29 11:33:04 +0300

    geometry module

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,11 +1,6 @@
-#!/usr/bin/python3
 """An empty class"""
 
 class BaseGeometry:
     """An empty class for BaseGeometry"""
     pass
-bg = BaseGeometry()
 
-print(bg)
-print(dir(bg))
-print(dir(BaseGeometry))
