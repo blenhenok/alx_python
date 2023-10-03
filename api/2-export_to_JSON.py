9dcb4fa455497474bc40d8c8914b6265c80f8aed
blen <blenhenok93@gmail.com>
2023-10-03 11:51:12 +0300

    export to json

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -4,27 +4,31 @@ import json
 import requests
 import sys
 
-
 if __name__ == "__main__":
     url = 'https://jsonplaceholder.typicode.com/'
 
+    # Get user ID from command line arguments
     userid = sys.argv[1]
+
+    # Fetch user data from API
     user = '{}users/{}'.format(url, userid)
     res = requests.get(user)
     json_o = res.json()
-    name = json_o.get('username')
+    username = json_o.get('username')  # Store username
 
+    # Fetch todos for user from API
     todos = '{}todos?userId={}'.format(url, userid)
     res = requests.get(todos)
     tasks = res.json()
+
+    # For each todo, store relevant data in l_task
     l_task = []
     for task in tasks:
-        dict_task = {"task": task.get('title'),
-                     "completed": task.get('completed'),
-                     "username": name}
-        l_task.append(dict_task)
+        l_task.append({"task": task.get('title'),
+                       "completed": task.get('completed'),
+                       "username": username})
 
-    d_task = {str(userid): l_task}
+    # Write data to JSON file
     filename = '{}.json'.format(userid)
-    with open(filename, mode='w') as f:
-        json.dump(d_task, f)
+    with open(filename, mode='w') as employee_file:
+        json.dump({userid: l_task}, employee_file)  # Write the dictionary to a JSON file
