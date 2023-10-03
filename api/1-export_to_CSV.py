9d910a0d23e2d633b0367f3f36f4b1ac1601bebd
blen <blenhenok93@gmail.com>
2023-10-03 11:29:20 +0300

    Export to CSV

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -4,19 +4,24 @@ import csv
 import requests
 import sys
 
-
 if __name__ == "__main__":
     url = 'https://jsonplaceholder.typicode.com/'
 
+    # Get user ID from command line arguments
     userid = sys.argv[1]
+
+    # Fetch user data from API
     user = '{}users/{}'.format(url, userid)
     res = requests.get(user)
     json_o = res.json()
-    name = json_o.get('username')
+    name = json_o.get('username')  # Store username
 
+    # Fetch todos for user from API
     todos = '{}todos?userId={}'.format(url, userid)
     res = requests.get(todos)
     tasks = res.json()
+
+    # For each todo, store relevant data in l_task
     l_task = []
     for task in tasks:
         l_task.append([userid,
@@ -24,6 +29,7 @@ if __name__ == "__main__":
                        task.get('completed'),
                        task.get('title')])
 
+    # Write data to CSV file
     filename = '{}.csv'.format(userid)
     with open(filename, mode='w') as employee_file:
         employee_writer = csv.writer(employee_file,
@@ -31,4 +37,4 @@ if __name__ == "__main__":
                                      quotechar='"',
                                      quoting=csv.QUOTE_ALL)
         for task in l_task:
-            employee_writer.writerow(task)
+            employee_writer.writerow(task)  # Write each list in l_task as a new row in the CSV file
